05b4e87c36667a415700f8310895e8fe8a4a73f8
Krabbenhoft <67756044+Krabbenhoft@users.noreply.github.com>
2025-02-18 10:37:07 -0500

    Update Coach.java

--- src/csu/csci325/Coach.java
+++ src/csu/csci325/Coach.java
@@ -4,5 +4,12 @@ package csu.csci325;
 
 
 public class Coach extends TeamMember{
+    int numRunners;
+    String coachingStyle;
     
+        public Runner (String name, String teamName, int age, int meetsAttended, int speed, int numRunners, String coachingStyle) {
+        super(name, teamName, age, meetsAttended);
+        this.numRunners=numRunners;
+        this.coachingStyle=coachingStyle;
+    }   
 }
